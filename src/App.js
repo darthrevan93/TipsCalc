5883c86eafa02c64af64b187359c8454120ae21b
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-05-05 03:16:01 -0300

    primeiro-commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import '/wamp/www/gitmodule/react1/src/App.css'
+import './components/contadorgorj.jsx'
+import Contador from './components/contadorgorj.jsx';
+import './App.css'
+
+
+
+function App() {
+  return (
+    <>
+      
+      <div>
+        <Contador></Contador>
+      </div>
+    </>
+  );
+}
+
+
+export default App;
---
+++ src/components/contadorgorj.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import '/wamp/www/gitmodule/react1/src/App.css'
+
+
+function Contador() {
+    const [ conta, setConta ] = useState('');
+        const HandleContaInput = (e) => {
+            setConta(e.target.value)
+        }           
+
+     
+    const [ gorjeta, setGorjeta ] = useState('');
+        const HandleGorjetaInput = (e) => {
+            setGorjeta(e.target.value)
+}
+
+    return (
+        <>
+            <div className='caixa1'>
+                <p>Indique o valor da conta:</p>
+            <input type='number' onChange={HandleContaInput} />    
+                <p>Indique a % da Gorjeta:</p>
+            <input type='number' onChange={HandleGorjetaInput} /><br/><br/>
+
+            <h2>Resultado do cálculo:</h2>
+            <p>O total da conta foi: {conta} R$</p>
+            <p>(+) Gorjeta: {gorjeta} %</p>
+            <h2>Total: <span>{parseFloat(conta) + (parseFloat(gorjeta) / 100) * parseFloat(conta) || 0} R$</span></h2>
+            </div>
+    
+
+        </>
+    )
+}    
+
+
+
+
+
+
+export default Contador;
